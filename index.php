10d208b323848cd735135ef19e790d6180f2b0d7
putri.amelia <194ameliaputri@gmail.com>
2026-04-13 13:57:40 +0700

    update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php echo "OK";
+<?php
+
+$uri = $_SERVER['REQUEST_URI'];
+
+// remove query string
+$uri = strtok($uri, '?');
+
+if ($uri === '/receive.php') {
+    require __DIR__ . '/receive.php';
+    return;
+}
+
+// fallback test
+echo "OK";
